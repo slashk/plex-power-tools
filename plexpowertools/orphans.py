f89bb9f20bd62e5425b17546fa4fe692ba223d1a
Ken Pepple <ken.pepple@rabbityard.com>
2012-09-23 11:07:37 -0700

    added LICENSE. reorg'd orphans.

--- plexpowertools/orphans.py
+++ plexpowertools/orphans.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012, Ken Pepple
+#
+# All rights reserved.
 
 import fnmatch
 import os
-import sqlite3
+import sys
 
+try:
+  import sqlite3
+except:
+  print "ERROR: you need to install the necessary python eggs."
+  print "$ sudo easy_install sqlite3"
+  sys.exit(1)
+
+
+directory = "/opt/video/tv/"
+db = "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
 
 def walk_video_directory(filepath):
   pattern = '[!._]*'
@@ -13,18 +28,19 @@ def walk_video_directory(filepath):
       x.append(os.path.join(root,filename))
   return x
 
-directory = "/opt/video/tv/"
-db = "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
+def usage():
+  pass
 
-conn = sqlite3.connect(db)
-c = conn.cursor()
-videofiles = walk_video_directory(directory)
-for x in videofiles:
-  c.execute('select media_item_id from media_parts where file=?', (x,))
-  result = c.fetchone()
-  #print result
-  #print "%s: %s" % (x, str(result[0])) 
-  if result is None:
-    print 'rm "%s"' % x 
-  #else:
-    #print result
+def main(argv):
+  conn = sqlite3.connect(db)
+  c = conn.cursor()
+  videofiles = walk_video_directory(directory)
+  for x in videofiles:
+    c.execute('select media_item_id from media_parts where file=?', (x,))
+    result = c.fetchone()
+    if result is None:
+      print '%s' % x 
+
+
+if __name__ == '__main__':
+  main(sys.argv)
